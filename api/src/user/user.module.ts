b74b92f2a0152ec53683151a039d8fc011380be1
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-07-29 21:43:13 +0300

    added controller to module

--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserService } from './service/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { userEntity } from './models/user.entity';
+import { UserController } from './controller/user.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([userEntity])
   ],
-  providers: [UserService]
+  providers: [UserService],
+  controllers: [UserController] 
 })
 export class UserModule {}
