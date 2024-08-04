67a83f85ddc113f363000cb8661d1ce3dee0b3ca
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-04 22:36:29 +0300

    added passwordMatch validation

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -14,45 +14,55 @@ class CustomValidators {
       return { passwordInvalid: true };
     }
   }
+
+  static passwordMatch(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if ((password === confirmPassword) && password !== null && confirmPassword !== null) {
+      return null;
+    } else {
+      return { passwordsNotMatching: true };
+    }
+  }
 }
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss'] // corrected to styleUrls
 })
 export class RegisterComponent implements OnInit {
-
   registerForm: FormGroup = new FormGroup({});
 
   constructor(
     private authService: AuthenticationService,
     private formBuilder: FormBuilder,
     private router: Router,
-  ){}
+  ) { }
 
   ngOnInit(): void {
-      this.registerForm = this.formBuilder.group({
-        name: [null, [Validators.required]],
-        username: [null, [Validators.required]],
-        email: [null, [
-          Validators.required,
-          Validators.email,
-          Validators.minLength(6),
-        ]],
-        password: [null, [
-          Validators.required,
-          Validators.minLength(3),
-          CustomValidators.passwordContainsNumber
-        ]],
-        passwordConfirm: [ null, [Validators.required]]
-      }), {
-        // validators: CustomValidators.passwordMatches
-      }
+    this.registerForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      username: [null, [Validators.required]],
+      email: [null, [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(6),
+      ]],
+      password: [null, [
+        Validators.required,
+        Validators.minLength(3),
+        CustomValidators.passwordContainsNumber
+      ]],
+      confirmPassword: [null, [Validators.required]]
+    }, {
+      validators: CustomValidators.passwordMatch
+    });
   }
 
-  onSubmit(){
-    if(this.registerForm.invalid){
+  onSubmit() {
+    if (this.registerForm.invalid) {
       return;
     }
     console.log(this.registerForm.value);
@@ -60,5 +70,4 @@ export class RegisterComponent implements OnInit {
       map(user => this.router.navigate(["login"]))
     ).subscribe();
   }
-
 }
