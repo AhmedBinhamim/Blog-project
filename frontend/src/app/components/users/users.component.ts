1740be2eeb2f6bca4335c6c750c4e59566be7b5e
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-08 19:50:33 +0300

    not needed dependency

--- frontend/src/app/components/users/users.component.ts
+++ frontend/src/app/components/users/users.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserData, UserService } from '../../services/user.service';
-import { map, tap } from 'rxjs';
+import { map} from 'rxjs';
 import { PageEvent } from '@angular/material/paginator';
 
 @Component({
