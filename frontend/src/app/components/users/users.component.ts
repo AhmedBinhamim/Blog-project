b60810f50cbd9ac4d222ef216173e68cde26420c
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-08 16:25:52 +0300

    added datatable

--- frontend/src/app/components/users/users.component.ts
+++ frontend/src/app/components/users/users.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserData, UserService } from '../../services/user.service';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
 
+  dataSource: UserData | null = null;
+  displayedColumns: string[] = ['id', 'name', 'username', 'email', 'role'];
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {    
+   this.initDataSource();
+  }
+
+  initDataSource(){
+    this.userService.findAll(1, 10).pipe(
+      tap(users => console.log(users)),
+      map((userData: UserData) => this.dataSource = userData)
+    ).subscribe();
+  }
 }
