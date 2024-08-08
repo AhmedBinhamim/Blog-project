ddda9f5abee5d95cfeb1c75516e69d1a1cc17eea
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-08 20:05:27 +0300

    added query param to users endpoint

--- api/src/user/controller/user.controller.ts
+++ api/src/user/controller/user.controller.ts
@@ -39,8 +39,10 @@ export class UserController {
     index(
         @Query('page') page: number = 1,
         @Query('limit') limit: number = 10,
+        @Query('username') username: string,
     ): Observable<Pagination<User>>{
         limit = limit > 100 ? 100 : limit;
+        console.log(username)
         return this.userService.paginate({page: Number(page), limit: Number(limit), route: 'http://localhost:3000/api/users' })
     }
 
