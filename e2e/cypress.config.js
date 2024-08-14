3a1371141f0739c4d10abaf6c7f651c3fab7cc8d
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-14 16:41:40 +0300

    added mochawesome test reports

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -3,9 +3,15 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
     baseUrl: "http://localhost:4200",
     specPattern: "cypress/integration/**/*.spec.{js,ts}",
+    reporter: 'mochawesome',
+    reporterOptions: {
+      overwrite: false,
+      html: false,
+      json: true,
+    },
   },
 });
