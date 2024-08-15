c87371bc0770f1f8a13629f5f105a31d7f6874e0
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-15 22:25:37 +0300

    added logic to user-profile component

--- frontend/src/app/components/user-profile/user-profile.component.ts
+++ frontend/src/app/components/user-profile/user-profile.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { map, Subscription } from 'rxjs';
+import { User } from '../../services/authentication service/authentication.service';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user service/user.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit, OnDestroy {
 
+  userId: number | null = null;
+  private sub: Subscription | undefined;
+  user: User | null = null;
+
+
+  constructor(
+    private activatedRouter: ActivatedRoute,
+    private userService: UserService
+  ){}
+
+  ngOnInit(): void {
+      this.sub = this.activatedRouter.params.subscribe(params => {
+        this.userId = parseInt(params['id']);
+        this.userService.findOne(this.userId).pipe(
+          map((user:User) => this.user = user)
+        ).subscribe()
+      })
+  }
+
+  ngOnDestroy(){
+    this.sub?.unsubscribe();
+  }
 }
