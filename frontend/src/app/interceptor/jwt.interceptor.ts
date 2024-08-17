f116cc0a941c4ee31ab25437a8fe55f581db5caa
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-17 18:20:15 +0300

    fixed small issue regarding jwt interceptor

--- frontend/src/app/interceptor/jwt.interceptor.ts
+++ frontend/src/app/interceptor/jwt.interceptor.ts
@@ -1,23 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 import { JWT_NAME } from '../services/authentication service/authentication.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem(JWT_NAME);
-    
-    if (token) {
-      const clonedReq = req.clone({
-        headers: req.headers.set('Authorization',
-          'Bearer' + token
-        )
-      });
-     
-      return next.handle(clonedReq);
-    } else {
-      return next.handle(req);
+    let clonedReq = req;
+
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem(JWT_NAME);
+      if (token) {
+        clonedReq = req.clone({
+          headers: req.headers.set('Authorization', `Bearer ${token}`)
+        });
+      }
     }
+
+    return next.handle(clonedReq);
   }
 }
