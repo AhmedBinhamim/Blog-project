5f457f12085ca8708593af0896e432d153187491
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-18 17:50:05 +0300

    small auth locatstorage fix

--- frontend/src/app/services/authentication service/authentication.service.ts
+++ frontend/src/app/services/authentication service/authentication.service.ts
@@ -1,56 +1,72 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { map, Observable, of, switchMap, tap } from 'rxjs';
-import {JwtHelperService} from '@auth0/angular-jwt'
+import { JwtHelperService } from '@auth0/angular-jwt';
 
-export interface LoginForm{
+export interface LoginForm {
   email: string;
   password: string;
-};
+}
 
-export interface User{
-  id?: number,
+export interface User {
+  id?: number;
   name?: string;
   username?: string;
   email?: string;
   role?: string;
   password?: string;
-  //passwordConfirm: string;
-  profileImage?:string;
+  profileImage?: string;
 }
 
 export const JWT_NAME = 'blog-token';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  constructor(
+    private http: HttpClient,
+    private jwtHelper: JwtHelperService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
-  constructor(private http: HttpClient, private jwtHelper: JwtHelperService) {}
+  private isLocalStorageAvailable(): boolean {
+    return isPlatformBrowser(this.platformId) && typeof localStorage !== 'undefined';
+  }
 
-  login(loginForm: LoginForm){
-    return this.http.post<any>('/api/users/login', {email: loginForm.email, password: loginForm.password}).pipe(
+  login(loginForm: LoginForm): Observable<any> {
+    return this.http.post<any>('/api/users/login', { email: loginForm.email, password: loginForm.password }).pipe(
       map((token) => {
-        localStorage.setItem(JWT_NAME, token.access_token)
+        if (this.isLocalStorageAvailable()) {
+          localStorage.setItem(JWT_NAME, token.access_token);
+        }
         return token;
       })
-    )
+    );
   }
 
-  register(user: User){
+  register(user: User): Observable<any> {
     return this.http.post<any>('/api/users/', user).pipe(
-      map(user => user)
-    )
+      map((newUser) => newUser)
+    );
   }
 
-  isAuthenticated():boolean{
-    const token = localStorage.getItem(JWT_NAME)
-    return this.jwtHelper.isTokenExpired(token);
+  isAuthenticated(): boolean {
+    if (!this.isLocalStorageAvailable()) {
+      return false;
+    }
+    const token = localStorage.getItem(JWT_NAME);
+    return token ? !this.jwtHelper.isTokenExpired(token) : false;
   }
 
   getUserId(): Observable<number | null> {
+    if (!this.isLocalStorageAvailable()) {
+      return of(null);
+    }
+
     return of(localStorage.getItem(JWT_NAME)).pipe(
-      switchMap((jwt: string | null) => 
+      switchMap((jwt: string | null) =>
         of(jwt).pipe(
           map((token: string | null) => {
             if (!token) {
@@ -59,10 +75,9 @@ export class AuthenticationService {
             const decodedToken = this.jwtHelper.decodeToken(token);
             return decodedToken.user.id;
           }),
-          tap((jwt) => console.log(jwt))
+          tap((userId) => console.log(userId))
         )
       )
     );
   }
-  
 }
