5573318a2fabdaa1a96c9acd9ad2c44e1cb7d418
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-19 22:40:49 +0300

    added create blog endpoint

--- api/src/blog/blog.module.ts
+++ api/src/blog/blog.module.ts
@@ -3,8 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogEntryEntity } from './model/blog-entry.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
-import { BlogController } from './blog/blog.controller';
-import { BlogService } from './blog/blog.service';
+import { BlogController } from './controller/blog.controller';
+import { BlogService } from './service/blog.service';
 
 @Module({
     imports: [
--- api/src/blog/controller/blog.controller.ts
+++ api/src/blog/controller/blog.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request } from '@nestjs/common';
+import { BlogService } from '../service/blog.service';
+import { Observable } from 'rxjs';
+import { BlogEntry } from '../model/blog-entry.interface';
 
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+
+    constructor(private blogservice: BlogService){}
+
+    @Post()
+    create(@Body()blogEntry: BlogEntry, @Request() req): Observable<BlogEntry>{
+        const user = req.user.user;
+        return this.blogservice.create(user, blogEntry);
+    }
+}
