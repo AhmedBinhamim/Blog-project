0320fedbe08bbeed2dd6fd8d01205872674b5a99
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-19 22:48:04 +0300

    added create service and slugify

--- api/src/blog/service/blog.service.ts
+++ api/src/blog/service/blog.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { from, Observable, of, switchMap } from 'rxjs';
+import { BlogEntry } from '../model/blog-entry.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BlogEntryEntity } from '../model/blog-entry.entity';
+import { Repository } from 'typeorm';
+import { UserService } from 'src/user/service/user.service';
+import { User } from 'src/user/models/user.interface';
+
+const slugify = require('slugify');
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+
+    constructor(
+        @InjectRepository(BlogEntryEntity) private readonly blogRepository: Repository<BlogEntry>,
+        private userService: UserService
+    ){}
+
+    create(user: User, blogEntry: BlogEntry): Observable<BlogEntry>{
+        blogEntry.author = user;
+        return this.generateSlug(blogEntry.title).pipe(
+            switchMap((slug: string) => {
+                blogEntry.slug = slug;
+                return from(this.blogRepository.save(blogEntry))
+            })
+        )
+    }
+
+    generateSlug(title: string): Observable<string>{
+        return of(slugify(title));
+    }
+}
