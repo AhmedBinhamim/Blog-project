10a8865cfcfe3f1fc3d73cfde1c9e6f11ec095d1
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-19 22:49:10 +0300

    added auth guard to create endpoint

--- api/src/blog/controller/blog.controller.ts
+++ api/src/blog/controller/blog.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post, Request } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { BlogService } from '../service/blog.service';
 import { Observable } from 'rxjs';
 import { BlogEntry } from '../model/blog-entry.interface';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
 
 @Controller('blog')
 export class BlogController {
 
     constructor(private blogservice: BlogService){}
 
+    @UseGuards(JwtAuthGuard)
     @Post()
     create(@Body()blogEntry: BlogEntry, @Request() req): Observable<BlogEntry>{
         const user = req.user.user;
