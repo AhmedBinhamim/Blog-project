e2bc8d90eee9eb37e5c6c3f7c80cdd96eb9351cc
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-19 22:33:50 +0300

    added blog controller

--- api/src/blog/blog.module.ts
+++ api/src/blog/blog.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogEntryEntity } from './model/blog-entry.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
+import { BlogController } from './blog/blog.controller';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([BlogEntryEntity]),
         AuthModule,
         UserModule,
-    ]
+    ],
+    controllers: [BlogController]
 })
 export class BlogModule {}
