fed73163f506dcadc5aa97212faa5b6f68fff109
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-19 23:43:16 +0300

    added blogentries relations to users

--- api/src/user/service/user.service.ts
+++ api/src/user/service/user.service.ts
@@ -37,7 +37,7 @@ export class UserService {
   }
 
   findOne(id: number): Observable<User> {
-    return from(this.userRepository.findOne({ where: { id } })).pipe(
+    return from(this.userRepository.findOne({ where: { id }, relations:['blogEntries'] })).pipe(
       map((user: User) => {
         const { password, ...result } = user;
         return result;
