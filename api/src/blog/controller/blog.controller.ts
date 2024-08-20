186e873bff0ab51436fefd6cba10608c5f8c09ca
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-20 20:28:40 +0300

    added userisauthor guard to update one blog

--- api/src/blog/controller/blog.controller.ts
+++ api/src/blog/controller/blog.controller.ts
@@ -3,6 +3,7 @@ import { BlogService } from '../service/blog.service';
 import { Observable } from 'rxjs';
 import { BlogEntry } from '../model/blog-entry.interface';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
+import { UserIsAuthorGuard } from '../guards/user-is-author.guard';
 
 @Controller('blogs')
 export class BlogController {
@@ -32,7 +33,7 @@ export class BlogController {
     }
 
 
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, UserIsAuthorGuard)
     @Put(':id')
     updateOne(@Param('id') id: number, @Body() blogEntry: BlogEntry): Observable<BlogEntry>{
         return this.blogService.updateOne(Number(id), blogEntry);
