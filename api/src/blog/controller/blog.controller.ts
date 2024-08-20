0b63f5b71ca9404699dd57da69d8158e1d6b4e18
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-20 20:27:46 +0300

    new endpoint update one blog

--- api/src/blog/controller/blog.controller.ts
+++ api/src/blog/controller/blog.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query, Request, UseGuards } from '@nestjs/common';
 import { BlogService } from '../service/blog.service';
 import { Observable } from 'rxjs';
 import { BlogEntry } from '../model/blog-entry.interface';
@@ -7,27 +7,34 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
 @Controller('blogs')
 export class BlogController {
 
-    constructor(private blogservice: BlogService){}
+    constructor(private blogService: BlogService){}
 
     @UseGuards(JwtAuthGuard)
     @Post()
     create(@Body()blogEntry: BlogEntry, @Request() req): Observable<BlogEntry>{
         const user = req.user;
-        return this.blogservice.create(user, blogEntry);
+        return this.blogService.create(user, blogEntry);
     }
 
     @Get()
     findBlogEntries(@Query('userId') userId: number): Observable<BlogEntry[]>{
         if(userId == null){
-            return this.blogservice.findAll();
+            return this.blogService.findAll();
         }
         else {
-            return this.blogservice.findByUser(userId);
+            return this.blogService.findByUser(userId);
         }
     }
 
     @Get(':id')
     findOne(@Param('id') id: number): Observable<BlogEntry>{
-        return this.blogservice.findOne(id)
+        return this.blogService.findOne(id)
+    }
+
+
+    @UseGuards(JwtAuthGuard)
+    @Put(':id')
+    updateOne(@Param('id') id: number, @Body() blogEntry: BlogEntry): Observable<BlogEntry>{
+        return this.blogService.updateOne(Number(id), blogEntry);
     }
 }
