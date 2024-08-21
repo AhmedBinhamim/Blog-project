cd5861151f4012bfe5806179ee732c379d549bdd
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-21 18:25:03 +0300

    paginate blogs by user

--- api/src/blog/controller/blog.controller.ts
+++ api/src/blog/controller/blog.controller.ts
@@ -5,6 +5,8 @@ import { BlogEntry } from '../model/blog-entry.interface';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
 import { UserIsAuthorGuard } from '../guards/user-is-author.guard';
 
+export const BLOG_ENTRIES_URL = 'http://localhost:3000/api/blog-entries';
+
 @Controller('blog-entries')
 export class BlogController {
 
@@ -37,7 +39,7 @@ export class BlogController {
         return this.blogService.paginateAll({
             limit: Number(limit),
             page: Number(page),
-            route: 'http://localhost:3000/api/blog-entries'
+            route: BLOG_ENTRIES_URL
         })
     }
 
@@ -45,6 +47,22 @@ export class BlogController {
     findOne(@Param('id') id: number): Observable<BlogEntry>{
         return this.blogService.findOne(id)
     }
+
+    @Get('user/:user')
+    indexByUser(
+        @Query('page') page: number = 1,
+        @Query('limit') limit: number = 10,
+        @Param('user') userId: number,
+    ){
+        limit = limit > 100 ? 100 : limit;
+
+        return this.blogService.paginateByUser({
+            limit: Number(limit),
+            page: Number(page),
+            route: BLOG_ENTRIES_URL
+        }, Number(userId))
+    }
+
 
 
     @UseGuards(JwtAuthGuard, UserIsAuthorGuard)
