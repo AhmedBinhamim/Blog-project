8414d100d76282b9095ed60f2a1d3eb5111b96d2
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-22 18:46:14 +0300

    added paginate on change

--- frontend/src/app/components/all-blog-entries/all-blog-entries.component.ts
+++ frontend/src/app/components/all-blog-entries/all-blog-entries.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BlogEntriesPageable } from '../../model/blog-entry.interface';
 import { BlogService } from '../../services/blog service/blog.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-all-blog-entries',
@@ -11,14 +12,15 @@ import { BlogService } from '../../services/blog service/blog.service';
 export class AllBlogEntriesComponent implements OnInit {
 
   dataSource!: Observable<BlogEntriesPageable>;
-  
+  pageEvent: PageEvent | undefined;
+
   constructor(private blogService: BlogService) {}
 
   ngOnInit(): void {
     this.dataSource = this.blogService.indexAll(1, 10);
   }
 
-  onPaginateChange(){
-    
+  onPaginateChange(event: PageEvent){
+
   }
 }
