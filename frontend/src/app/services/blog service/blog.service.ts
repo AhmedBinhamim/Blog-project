c24fbd27442e7a90cbdca00a8fbf1b790226527e
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-22 18:37:52 +0300

    added service to get all blog entries

--- frontend/src/app/services/blog service/blog.service.ts
+++ frontend/src/app/services/blog service/blog.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogEntriesPageable } from '../../model/blog-entry.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  indexAll(page: number, size: number): Observable<BlogEntriesPageable>{
+    let params = new HttpParams();
+
+    params = params.append('page', String(page));
+    params = params.append('size', String(size));
+
+    return this.http.get<BlogEntriesPageable>('/api/blog-entries', {params});
+  }
 }
