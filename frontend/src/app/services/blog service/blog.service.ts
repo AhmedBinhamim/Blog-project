1ea084b8c8ad9127b9b92ca96d8eb0db0579e414
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-22 20:00:16 +0300

    added api call to get blog entries

--- frontend/src/app/services/blog service/blog.service.ts
+++ frontend/src/app/services/blog service/blog.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { BlogEntriesPageable } from '../../model/blog-entry.interface';
 
 @Injectable({
@@ -16,6 +16,6 @@ export class BlogService {
     params = params.append('page', String(page));
     params = params.append('size', String(size));
 
-    return this.http.get<BlogEntriesPageable>('/api/blog-entries', {params});
+    return this.http.get<BlogEntriesPageable>('/api/blog-entries', {params})
   }
 }
