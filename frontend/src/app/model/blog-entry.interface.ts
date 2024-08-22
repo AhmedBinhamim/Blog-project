8c2d889c4a43d791255ba7dc635a0b103da2bc52
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-22 18:34:01 +0300

    added meta, ling and blog entries pageable interface

--- frontend/src/app/model/blog-entry.interface.ts
+++ frontend/src/app/model/blog-entry.interface.ts
@@ -14,3 +14,24 @@ export interface BlogEntry {
     publishedDate?: Date;
     isPublished?: boolean;
 }
+
+export interface Meta {
+    totalItems: number;
+    itemCount: number;
+    itemsPerPage: number;
+    totalPages: number;
+    currentPage: number;
+}
+
+export interface Links {
+    first: string;
+    previous: string;
+    next: string;
+    last: string;
+}
+
+export interface BlogEntriesPageable{
+    items: BlogEntry[],
+    meta: Meta,
+    links: Links,
+}
