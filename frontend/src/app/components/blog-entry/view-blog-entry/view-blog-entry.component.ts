c29206304b0f76244829144508e8f9b943933598
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-25 22:51:11 +0300

    add logic to view blog entry

--- frontend/src/app/components/blog-entry/view-blog-entry/view-blog-entry.component.ts
+++ frontend/src/app/components/blog-entry/view-blog-entry/view-blog-entry.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { map, Observable, switchMap } from 'rxjs';
+import { BlogEntry } from '../../../model/blog-entry.interface';
+import { BlogService } from '../../../services/blog service/blog.service';
 
 @Component({
   selector: 'app-view-blog-entry',
   templateUrl: './view-blog-entry.component.html',
-  styleUrl: './view-blog-entry.component.scss'
+  styleUrls: ['./view-blog-entry.component.scss']
 })
-export class ViewBlogEntryComponent {
+export class ViewBlogEntryComponent implements OnInit {
+  
+  blogEntry!: Observable<BlogEntry>;
 
+  constructor(private activatedRouter: ActivatedRoute, private blogService: BlogService) {}
+
+  ngOnInit(): void {
+    this.blogEntry = this.activatedRouter.params.pipe(
+      switchMap((params: Params) => {
+        const blogEntryId: number = parseInt(params['id']);
+        return this.blogService.findOne(blogEntryId).pipe(
+          map((blogEntry: BlogEntry) => blogEntry)
+        );
+      })
+    );
+  }
 }
