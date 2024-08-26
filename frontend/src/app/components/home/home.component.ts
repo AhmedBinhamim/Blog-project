107afae234e9c42c9416e898544bbcf4c08070e7
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-26 17:01:23 +0300

    use blog entries to populate home

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Observable } from 'rxjs';
+import { BlogEntriesPageable } from '../../model/blog-entry.interface';
+import { BlogService } from '../../services/blog service/blog.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  blogEntries$: Observable<BlogEntriesPageable> | undefined;
+  constructor(private blogService: BlogService){}
+
+  ngOnInit(): void {
+    this.blogEntries$ = this.blogService.indexAll(1, 10);
+  }
+
+  onPaginateChange(event: PageEvent){
+    this.blogEntries$ = this.blogService.indexAll(event.pageIndex, event.pageSize);
+  }
 }
