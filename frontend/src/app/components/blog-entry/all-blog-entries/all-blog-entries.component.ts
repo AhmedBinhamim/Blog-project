e33c45f39a7f1593dd7e33fd3415035a013e3b29
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-28 20:52:54 +0300

    fix origin issue

--- frontend/src/app/components/blog-entry/all-blog-entries/all-blog-entries.component.ts
+++ frontend/src/app/components/blog-entry/all-blog-entries/all-blog-entries.component.ts
@@ -1,37 +1,45 @@
-import { Component, Input, OnInit, Output, EventEmitter, Inject } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, Inject, PLATFORM_ID } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PageEvent } from '@angular/material/paginator';
 import { BlogEntriesPageable } from '../../../model/blog-entry.interface';
 import { Router } from '@angular/router';
 import { WINDOW } from '../../../window-token';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-all-blog-entries',
   templateUrl: './all-blog-entries.component.html',
   styleUrls: ['./all-blog-entries.component.scss']
 })
-export class AllBlogEntriesComponent {
+export class AllBlogEntriesComponent implements OnInit {
 
-  origin: string;
-  @Input() blogEntries!: BlogEntriesPageable;
+  origin: string = '';
+  @Input() blogEntries: BlogEntriesPageable | null = null;
   @Output() paginate: EventEmitter<PageEvent> = new EventEmitter<PageEvent>();
 
-
-  
   pageEvent: PageEvent | undefined;
 
-  constructor(private router: Router,  @Inject(WINDOW) private window: Window) {
-    this.origin = this.window.location.origin;
-  }
+  constructor(
+    private router: Router,
+    @Inject(WINDOW) private window: Window,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.origin = this.window.location.origin;
+    } else {
+      this.origin = '';
+    }
+  }
   
 
-  onPaginateChange(event: PageEvent){
-    event.pageIndex = event.pageIndex + 1
+  onPaginateChange(event: PageEvent) {
+    event.pageIndex = event.pageIndex + 1;
     this.paginate.emit(event);
   }
 
-  navigate(id: any){
+  navigate(id: any) {
     this.router.navigateByUrl('blog-entries/' + id);
   }
 }
