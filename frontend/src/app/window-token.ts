1e4a2a24d735228a7e0fc83e232dc247da19aff2
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-28 20:51:54 +0300

    fix window token issue

--- frontend/src/app/window-token.ts
+++ frontend/src/app/window-token.ts
@@ -1,12 +1,22 @@
-import { InjectionToken, FactoryProvider } from '@angular/core';
+import { InjectionToken, FactoryProvider, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 export const WINDOW = new InjectionToken<Window>('window');
 
+const windowFactory = (platformId: Object): Window | Object => {
+  if (isPlatformBrowser(platformId)) {
+    return window;
+  } else {
+    return {};
+  }
+};
+
 const windowProvider: FactoryProvider = {
   provide: WINDOW,
-  useFactory: () => window
+  useFactory: windowFactory,
+  deps: [PLATFORM_ID]
 };
 
 export const WINDOW_PROVIDERS = [
-    windowProvider
-]
+  windowProvider
+];
