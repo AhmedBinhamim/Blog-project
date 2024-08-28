32dac6a2aa44b52e1c1a451b080c50eef66761a0
Ahmed Hassan Binhamim <126442288+AhmedBinhamim@users.noreply.github.com>
2024-08-28 20:52:31 +0300

    fix origin issue

--- frontend/src/app/components/blog-entry/view-blog-entry/view-blog-entry.component.ts
+++ frontend/src/app/components/blog-entry/view-blog-entry/view-blog-entry.component.ts
@@ -1,9 +1,10 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { map, Observable, switchMap } from 'rxjs';
 import { BlogEntry } from '../../../model/blog-entry.interface';
 import { BlogService } from '../../../services/blog service/blog.service';
 import { WINDOW } from '../../../window-token';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-view-blog-entry',
@@ -11,12 +12,19 @@ import { WINDOW } from '../../../window-token';
   styleUrls: ['./view-blog-entry.component.scss']
 })
 export class ViewBlogEntryComponent implements OnInit {
-  
-  origin: string;
+
+  origin: string = '';
   blogEntry!: Observable<BlogEntry>;
 
-  constructor(private activatedRouter: ActivatedRoute, private blogService: BlogService,  @Inject(WINDOW) private window: Window) {
-    this.origin = this.window.location.origin;
+  constructor(
+    private activatedRouter: ActivatedRoute, 
+    private blogService: BlogService,  
+    @Inject(WINDOW) private window: Window,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.origin = this.window.location.origin;
+    }
   }
 
   ngOnInit(): void {
